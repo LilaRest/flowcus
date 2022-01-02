a7efe5def07d2d0cb62d1a2f82d9b3e41a4fcb59
Lila Rest <mail@lila.rest>
2022-01-02 16:43:06 +0100

    First vesion of the option page

--- frontend/views/clutter_free/extractors/medium.js
+++ frontend/views/clutter_free/extractors/medium.js
@@ -23,9 +23,8 @@
       content: {
         selectors: ['article'],
 
-        // Is there anything in the content you selected that needs transformed
-        // before it's consumable content? E.g., unusual lazy loaded images
         transforms: {
+
           // Re-write lazy-loaded youtube videos
           iframe: $node => {
             const ytRe = /https:\/\/i.embed.ly\/.+url=https:\/\/i\.ytimg\.com\/vi\/(\w+)\//;
@@ -63,9 +62,6 @@
           },
         },
 
-        // Is there anything that is in the result that shouldn't be?
-        // The clean selectors will remove anything that matches from
-        // the result
         clean: ['span:not(pre > span)', 'svg'],
       },
 
