01fcc391a5aeef1e6c038a4c84cc838036ac0c06
Lila Rest <mail@lila.rest>
2022-01-02 18:45:46 +0100

    Improve the options page + create the options/main.js script that allows to save options in obrowser's storage

--- options/main.js
+++ options/main.js
@@ -1,21 +1,73 @@
-// function saveOptions(e) {
-//   browser.storage.sync.set({
-//     colour: document.querySelector("#colour").value
-//   });
-//   e.preventDefault();
-// }
-//
-// function restoreOptions() {
-//   var storageItem = browser.storage.managed.get('colour');
-//   storageItem.then((res) => {
-//     document.querySelector("#managed-colour").innerText = res.colour;
-//   });
-//
-//   var gettingItem = browser.storage.sync.get('colour');
-//   gettingItem.then((res) => {
-//     document.querySelector("#colour").value = res.colour || 'Firefox red';
-//   });
-// }
-//
-// document.addEventListener('DOMContentLoaded', restoreOptions);
-// document.querySelector("form").addEventListener("submit", saveOptions);
+
+function init () {
+
+    const labels = document.querySelectorAll("form label");
+    const fields = [];
+    for (const label of labels) {
+        fields.push(label.nextElementSibling)
+    }
+    const saved = document.querySelector("#saved")
+
+    function saveOptions(e) {
+
+        e.preventDefault();
+
+        // Retrieve fields datas.
+        let datas = {};
+        for (const field of fields) {
+            if (field.tagName === "INPUT" && field.type === "checkbox") {
+                datas[field.name] = field.checked
+            }
+            else {
+                datas[field.name] = field.value
+            }
+        }
+
+        // Store datas in browser storage.
+        browser.storage.sync.set(datas);
+
+        // Add a saved message next to the field.
+        const saved_message = document.createElement("span")
+        saved_message.id = "saved-message"
+        saved_message.innerText = "Saved 🗸"
+        saved_message.style.opacity = "1"
+
+        this.parentElement.appendChild(saved_message)
+        setTimeout(function () {
+            saved_message.style.opacity = "0"
+            setTimeout(function () {
+                this.parentElement.removeChild(saved_message)
+            }.bind(this), 500)
+        }.bind(this), 2000)
+    }
+
+    function restoreOptions() {
+
+        for (const field of fields) {
+
+            function setValue(result) {
+                if (field.tagName === "INPUT" && field.type === "checkbox") {
+                    field.checked = result[field.name]
+                }
+                else {
+                    field.value = result[field.name]
+                }
+            }
+
+            function onError (error) {
+                console.log(`Error: ${error}`);
+            }
+
+            const getting = browser.storage.sync.get(field.name).then(setValue, onError)
+        }
+    }
+
+    restoreOptions()
+
+    for (const field of fields) {
+        field.addEventListener("change", saveOptions.bind(field));
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', init);
