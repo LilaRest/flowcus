e998e880cc0b022646263e6af9592e414c10c330
Lila Rest <mail@lila.rest>
2022-01-07 01:37:08 +0100

    Fix medium websites that are not using <code> elements to display code blocks

--- frontend/views/clutter_free/fixers/medium.js
+++ frontend/views/clutter_free/fixers/medium.js
@@ -9,12 +9,28 @@ const medium_clutter_free_fixer = (function () {
 
                 // If a Medium article is loaded and if the user scroll down the page before activation Flowcus, he will trigger lazyloaded
                 // footer and the Mercury render will be broken.
-                // Isolating only the article's content fix this problem :
+                // Isolating only the article's content fix this problem.
+                // In addtion it improves the performance because Mercury will have less work (~30% faster)
                 const article = body.querySelector("div.s > article").cloneNode(true)
                 body.innerHTML = ""
                 body.appendChild(article)
 
+                // Fix code blocks that are not using <code> element in Medium powered websites.
+                const pres = body.querySelectorAll("div pre")
+                for (const pre of pres) {
+                   console.log(pre)
 
+                   const code_block = document.createElement("code")
+                   const pre_childs = pre.querySelectorAll("* :not(br)")
+
+                   for (const child of pre_childs) {
+                       try {
+                           code_block.innerHTML += child.innerHTML + "\n\x20\x20"
+                       } catch {}
+                   }
+                   pre.innerHTML = ""
+                   pre.appendChild(code_block)
+                }
 
                 // Fix line breaks in code blocks.
                 const code_blocks = body.querySelectorAll("code")
